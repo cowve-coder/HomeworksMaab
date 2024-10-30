0c12d2e424d8b48562ca01bd9499ccbd36e87078
cowve-coder <farruxjon1511@gmail.com>
2024-10-30 19:57:59 +0500

    not all of it is done!

---
+++ hw1/hw1.py
@@ -0,0 +1,126 @@
+import math
+#P1
+#a = float(input('input a number: '))
+#P = 4*a
+#print('perimeter is: ', P)
+
+#P2
+#a = float(input('input a number: '))
+#S = pow(a, 2)
+#print(S)
+
+#P3
+#a = float(input('enter a number: '))
+#b = float(input('enter a second number: '))
+#S = a * b
+#P = 2 * (a + b)
+#print('Area is: ', S, 'Perimeter is: ', P)
+
+#P4
+#p = 3.14
+#d = float(input('Enter a diameter of a circle: '))
+#L = d * p
+#print('length of the circle is: ', L)
+
+#P5
+#a = float(input('enter: '))
+#V = pow(a, 3)
+#S = 6 * pow(a, 2)
+#print('S = ', S, 'V = ', V)
+
+#P6
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#c = float(input('enter: '))
+#V = a * b * c
+#S = 2 * (a * b + b * c + c * a)
+#print('V = ', V, 'S = ', S)
+
+#P7
+#p = 3.14
+#r = float(input('enter: '))
+#L = 2 * p * r
+#S = p * pow(r, 2)
+#print('L = ', L, 'S =', S)
+
+#P8
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#avg = (a + b)/2
+#print('avg = ', avg)
+
+#P9
+#a = float(input('enter a nonnegative number: '))
+#b = float(input('enter a nonnegative number: '))
+#geometric_avg = math.sqrt(a * b)
+#print('Orta geometrik = ', geometric_avg)
+
+#P10
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#print(a + b, a * b, pow(a, 2), pow(b, 2))
+
+#P11
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#print(a + b, a * b, abs(a), abs(b))
+
+#P12
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#c = math.sqrt(pow(a, 2) + pow(b, 2))
+#P = a + b + c
+#print('perimeter = ', P, 'gipotenuza = ', c)
+
+#P13
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#p = 3.14
+#S1 = p * a
+#S2 = p * b
+#S3 = S1 - S2
+#print('S1 = ', S1, 'S2 = ', S2, 'S3 = ', S3)
+
+#P14
+#l = float(input('enter: '))
+#p = 3.14
+#r = l /(2 * p)
+#S = p * pow(r, 2)
+#print('radius = ', r, 'yuza = ', S)
+
+#P15
+#S = float(input('enter: '))
+#a = math.sqrt(S)
+#d = 2 * a
+#print(a, ' ', d)
+
+#P16
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#print(abs(a - b))
+
+#P17
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#c = float(input('enter: '))
+#AC = abs(a - c)
+#BC = abs(b - c)
+#print(AC + BC, ' ', abs(AC - BC))
+
+#P18
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#c = float(input('enter: '))
+#print((c - a) * (b - c))
+
+#P19
+
+
+#P20
+#a = float(input('enter: '))
+#b = float(input('enter: '))
+#c = float(input('enter: '))
+#d = float(input('enter: '))
+#print(math.sqrt((a - c)**2 + (b - d)**2))
+
+#P21
